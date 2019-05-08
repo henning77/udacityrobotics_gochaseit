4611038d978b42c390a6540a765cedf18736b6af
Henning Böger <henning.boeger@gmail.com>
2019-05-08 17:59:03 +0200

    Fixed drive_bot

--- ball_chaser/src/drive_bot.cpp
+++ ball_chaser/src/drive_bot.cpp
@@ -10,19 +10,17 @@ ros::Publisher motor_command_publisher;
 // This function publishes the requested linear x and angular velocities to the robot wheel joints
 // After publishing the requested velocities, a message feedback should be returned with the requested wheel velocities
 bool handle_drive_request(ball_chaser::DriveToTarget::Request& req,
-    simple_arm::GoToPosition::Response& res)
+    ball_chaser::DriveToTarget::Response& res)
 {
     ROS_INFO("DriveToTarget request received - j1:%1.2f, j2:%1.2f", (float)req.linear_x, (float)req.angular_z);
 
-    geometry_msgs::Twist motor_command;
-    
-    motor_command_publisher.publish(twist);
+    geometry_msgs::Twist motor_command;    
     motor_command.linear.x = req.linear_x;
     motor_command.angular.z = req.angular_z;
     motor_command_publisher.publish(motor_command);
 
     // Return a response message
-    res.msg_feedback = "Wheel velocities set - j1: " + std::to_string(joints_angles[0]) + " , j2: " + std::to_string(joints_angles[1]);
+    res.msg_feedback = "Wheel velocities set - linear.x: " + std::to_string(motor_command.linear.x) + " , angular.z: " + std::to_string(motor_command.angular.z);
     ROS_INFO_STREAM(res.msg_feedback);
 
     return true;
